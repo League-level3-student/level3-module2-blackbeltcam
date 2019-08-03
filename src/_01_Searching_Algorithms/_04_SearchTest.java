47e9525d494bcdd0915aac9c67648a840d295114
League Student <league@iMac-7.attlocal.net>
2019-08-02 17:41:12 -0700

    Finished Searching_Algorithms

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -24,16 +24,22 @@ class _04_SearchTest {
 		assertEquals(4, _01_BinarySearch.binarySearch(iq2, 0, 4, 536));
 	    assertEquals(-1, _01_BinarySearch.binarySearch(iq2, 0, 4, 5));
 	}
-//	
-//	@Test
-//	public void testInterpolationSearch() {
-//		//3. use the assertEquals method to test your interpolation search method.
-//		//   remember that the array must be sorted and evenly distributed
-//	}
-//	
-//	@Test
-//	public void testExponentialSearch() {
-//		//4. use the assertEquals method to test your exponential search method.
-//		//   remember that the array must be sorted
-//	}
+	
+	@Test
+	public void testInterpolationSearch() {
+		//3. use the assertEquals method to test your interpolation search method.
+		//   remember that the array must be sorted and evenly distributed
+		int[] iq3= {3, 6, 9, 12, 15};
+		assertEquals(-1, _02_InterpolationSearch.interpolationSearch(iq3, 10));
+		assertEquals(1, _02_InterpolationSearch.interpolationSearch(iq3, 6));
+	}
+
+	@Test
+	public void testExponentialSearch() {
+		//4. use the assertEquals method to test your exponential search method.
+		//   remember that the array must be sorted
+		int[] iq4= {4,32 ,45, 57, 536};
+		assertEquals(2, _03_ExponentialSearch.exponentialSearch(iq4, 45));
+	    assertEquals(-1, _03_ExponentialSearch.exponentialSearch(iq4, 94));
+	}
 }
